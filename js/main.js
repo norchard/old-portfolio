f9529058271adeefa33aa1f6ae5eccd87a2713b7
Nicole Orchard <norchard24@gmail.com>
2016-04-04 21:56:17 -0400

    Added Google Analyticsand cleaned up js

--- js/main.js
+++ js/main.js
@@ -29,35 +29,31 @@ function sendMessage() {
 
 
 function stickyFunction(){
-if (document.documentElement.clientWidth > 970) {
-    $(document).ready(function() {
-        $('.section').trigger('detach.ScrollToFixed');
-        var sections = $('.section');
-        sections.each(function(i) {
+    if (document.documentElement.clientWidth > 970) {
+        $(document).ready(function() {
+            $('.section').trigger('detach.ScrollToFixed');
+            var sections = $('.section');
+            sections.each(function(i) {
 
-            var section = $(sections[i]);
-            var next = sections[i + 1];
+                var section = $(sections[i]);
+                var next = sections[i + 1];
 
-            section.scrollToFixed({
-                marginTop: $('.header').outerHeight(true) + 10,
-                limit: function() {
-                    var limit = 0;
-                    if (next) {
-                        limit = $(next).offset().top - $(this).outerHeight(true) - 10;}
-                    // } else {
-                    //     limit = $('.footer').offset().top - $(this).outerHeight(true) - 10;
-                    // }
-                    return limit;
-                },
-                zIndex: 999
+                section.scrollToFixed({
+                    marginTop: $('.header').outerHeight(true) + 10,
+                    limit: function() {
+                        var limit = 0;
+                        if (next) {
+                            limit = $(next).offset().top - $(this).outerHeight(true) - 10;
+                        }
+                        return limit;
+                    },
+                    zIndex: 999
+                });
             });
         });
-    });
-}
-
-else {
-    $('.section').trigger('detach.ScrollToFixed');
-}
+    } else {
+        $('.section').trigger('detach.ScrollToFixed');
+    }
 }
 
 stickyFunction();
@@ -68,16 +64,12 @@ window.addEventListener("resize", stickyFunction);
 
 
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 500 ){
+    if ($(this).scrollTop() > 500) {
         $("#back").css('visibility', 'visible');
         document.getElementById("backimage").src="/img/top.png";
-    }
-
-    else {
+    } else {
      $("#back").css('visibility', 'hidden');
-
- }
-     
+    }
 });
 
 
